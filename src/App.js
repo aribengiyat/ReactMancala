eda4758722b4e240f6d6bdec994ab84d3688977f
Nicholas Cathcart <nicholascathcart@Nicholass-MacBook-Air.local>
2022-12-27 11:20:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
   )
 }
 function Board(){
-  let boardComponents = [];
+  //here we need to give this component a state that we can pass down to the store and pocket components through "props".
+
+  let boardComponents = []; //we will put all the board components in this array.
+
   boardComponents.push(<Store key="player-2-store" class="player-2-store"/>);
   for (let i=0; i<6; i++){
     boardComponents.push(<Pocket key={`player-2-pocket-${i}`} class={`player-2-pocket-${i}`}/>);
@@ -17,6 +20,9 @@ function Board(){
   for (let i=0; i<6; i++){
     boardComponents.push(<Pocket key={`player-1-pocket-${i}`} class={`player-1-pocket-${i}`}/>);
   }
+
+  
+
   return(
     <div className="board-grid">
       {boardComponents};
@@ -26,6 +32,7 @@ function Board(){
 
 function Store(props){
   return (
+    //the state that is passed down from Board will give the inner text to this div (it will go in between the tags). ie the number of marbles it holds. same for pocket below.
     <div className={props.class+ ' store'} >
     </div>
   )
